2f32c6497da89a2505c29c4e95b78b0e4dfee663
Bindu Kaladgi <bindukaladgi07@gmail.com>
2020-12-11 17:28:27 +0100

    understanding controlflow

--- main.go
+++ main.go
@@ -2,6 +2,24 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Print("wassappp la la la tingu tingu")
+func main() {
+	fmt.Println("Hello world")
+
+	foo()
+
+	for i := 1; i < 21; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+	}
+	bale()
+	fmt.Println("exit the code")
+}
+
+func foo() {
+	fmt.Println("I am in foo")
+}
+
+func bale() {
+	fmt.Println("I am in bar")
 }
